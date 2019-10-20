250a1300bdf3eaf2253b7116c3d54df3c36cdae2
steve9ll <qiuliaolin1@163.com>
2019-10-20 22:14:41 +0800

    add virtual-dom

--- day20/utils/helper.ts
+++ day20/utils/helper.ts
@@ -1,5 +1,8 @@
-function isSameType(oldVDom,newVDom){
-    return typeof oldVDom !== typeof newVDom ||
-        ((typeof oldVDom === 'string' || typeof oldVDom === 'number') && oldVDom !== newVDom) ||
-        oldVDom.tag !== newVDom.tag
+export function isSameType(oldVDom, newVDom) {
+  return (
+    typeof oldVDom !== typeof newVDom ||
+    ((typeof oldVDom === "string" || typeof oldVDom === "number") &&
+      oldVDom !== newVDom) ||
+    oldVDom.tag !== newVDom.tag
+  );
 }
