7161a2a5826b89a6702302374f193236db59c219
steve9II <qiuliaolin1@163.com>
2020-09-07 17:42:54 +0800

    day29(hexagon): achieve hexagon class

--- day29/hexagon.js
+++ day29/hexagon.js
@@ -1,40 +1,25 @@
 class HexagonCube {
-  x: number;
-  y: number;
-  z: number;
-  private static CUBE_DIRECTION: HexagonCube[] = [
-    new HexagonCube(+1, -1, 0),
-    new HexagonCube(+1, 0, -1),
-    new HexagonCube(0, +1, -1),
-    new HexagonCube(-1, +1, 0),
-    new HexagonCube(-1, 0, +1),
-    new HexagonCube(0, -1, +1),
-  ];
-  constructor(x: number, y: number, z: number) {
+  constructor(x, y, z) {
     this.x = x;
     this.y = y;
     this.z = z;
   }
-  static cubeAdd(center: HexagonCube, offset: HexagonCube) {
+  static cubeAdd(center, offset) {
     const x = center.x + offset.x;
     const y = center.y + offset.y;
     const z = center.z + offset.z;
     return new HexagonCube(x, y, z);
   }
-  static cubeScale(direction: HexagonCube, radius: number) {
+  static cubeScale(direction, radius) {
     const x = radius * direction.x;
     const y = radius * direction.y;
     const z = radius * direction.z;
     return new HexagonCube(x, y, z);
   }
-  static cubeNeighbor(cube: HexagonCube, direction: number) {
+  static cubeNeighbor(cube, direction) {
     return HexagonCube.cubeAdd(cube, this.CUBE_DIRECTION[direction]);
   }
-  static cubeRing(
-    center: HexagonCube,
-    radius: number,
-    startDirection: number
-  ): HexagonCube[] {
+  static cubeRing(center, radius, startDirection) {
     const results = [];
     const offset = HexagonCube.cubeScale(
       this.CUBE_DIRECTION[startDirection],
@@ -49,20 +34,31 @@ class HexagonCube {
     });
     return results;
   }
-  static cubeSpiral(
-    center: HexagonCube,
-    radius: number,
-    startDirection: number
-  ) {
+  static cubeSpiral(center, radius, startDirection) {
     const results = [center];
     for (let i = 0; i < radius; i++) {
       const cubeRings = HexagonCube.cubeRing(center, i, startDirection);
       results.push(...cubeRings);
     }
+    return results;
   }
-  static doubleheightToPixel(hex: HexagonCube, size: number): [number, number] {
+  static doubleheightToPixel(hex, size) {
     let x = ((size * 3) / 2) * hex.x;
-    let y = ((size * Math.sqrt(3)) / 2) * hex.z;
+    let y = size * ((Math.sqrt(3) / 2) * hex.x + Math.sqrt(3) * hex.z);
     return [x, y];
   }
+  static sizeToPlace(size) {
+    return {
+      width: 2 * size,
+      height: Math.sqrt(3) * size,
+    };
+  }
 }
+HexagonCube.CUBE_DIRECTION = [
+  new HexagonCube(+1, -1, 0),
+  new HexagonCube(+1, 0, -1),
+  new HexagonCube(0, +1, -1),
+  new HexagonCube(-1, +1, 0),
+  new HexagonCube(-1, 0, +1),
+  new HexagonCube(0, -1, +1),
+];
